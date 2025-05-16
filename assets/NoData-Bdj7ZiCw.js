bec1bf79453141dc278a691433f2c0acdab235fb
Alunara <>
2025-05-16 15:53:11 +0200

    New release

---
+++ assets/NoData-Bdj7ZiCw.js
@@ -0,0 +1,6 @@
+import{r as s,d as t,j as o,o as c,g as n,f as r,as as d,k as _,m as p,l}from"./index-Dn4CPbex.js";/**
+ * @license lucide-vue-next v0.511.0 - ISC
+ *
+ * This source code is licensed under the ISC license.
+ * See the LICENSE file in the root directory of this source tree.
+ */const i=s("search-x",[["path",{d:"m13.5 8.5-5 5",key:"1cs55j"}],["path",{d:"m8.5 8.5 5 5",key:"a8mexj"}],["circle",{cx:"11",cy:"11",r:"8",key:"4ej97u"}],["path",{d:"m21 21-4.3-4.3",key:"1qie3q"}]]),m={class:"no-data"},u={class:"message"},h=t({__name:"NoData",props:{message:{default:"No data available"}},setup(f){return(e,k)=>{const a=i;return c(),o("div",m,[n(a,{class:"icon"}),r("span",u,[d(e.$slots,"default",{},()=>[_(p(e.message||"No data available"),1)],!0)])])}}}),y=l(h,[["__scopeId","data-v-efdae361"]]);export{y as _};
