d57658bb1f52ed77416deb156b16c644a6c2f9c0
0xAlunara <88008691+0xAlunara@users.noreply.github.com>
2025-07-23 01:12:21 +0200

    New release

---
+++ assets/API.page-C-dPcI-D.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/Swagger-soIkxhwL.js","assets/index-DI-_SjLK.js","assets/index-Dqa2n4_e.css","assets/Swagger-CzH7p0Cr.css"])))=>i.map(i=>d[i]);
+import{d as o,b as a,o as n,h as p,p as s,q as t}from"./index-DI-_SjLK.js";const d=o({__name:"API.page",setup(r){const e=s(()=>t(()=>import("./Swagger-soIkxhwL.js"),__vite__mapDeps([0,1,2,3])));return(_,c)=>(n(),a(p(e),{url:"https://api.hippo.army/feeds-docs/openapi.json"}))}});export{d as default};
